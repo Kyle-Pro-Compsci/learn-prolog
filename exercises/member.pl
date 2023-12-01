218aee290c91cbc560d92830747d24c732a61cd7
Kyle Koo <kylekoo837@gmail.com>
2023-12-01 16:13:51 -0500

    member solution from tuesday

--- exercises/member.pl
+++ exercises/member.pl
@@ -1,3 +1,8 @@
 /**
 * Define the rules to test if an element X is a member of a list
 */
+# my attempt:
+# member(M, [A | T]) :- M == A or member(M, T). 
+
+member(X, [X | _]).
+member(X, [_ | T]) :- member(X, T).
